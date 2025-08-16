d53d03885e28f71cf71915ae23b669bed91af720
akshaytayade <tayade_akshay@outlook.com>
2025-08-16 18:11:27 +0530

    two_sum_2_sorted.py

---
+++ two_sum_2_sorted.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [r+1, l+1]
+                
+        return curSum
+
+# Input: numbers = [2,7,11,15], target = 9
+# Output: [1,2]
+# Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
